67f05e4e67c2ad82ebff13edf82a018a840618cc
Martin Goya <martin.goya@hotmail.com>
2016-12-08 15:08:49 -0300

    Now, every fifteen party has its own page,

---
+++ Oh lala Web/Controllers/Fifteen/FifteenController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oh_lala_Web.Controllers.Fifteen
+{
+    public class FifteenController : Controller
+    {
+        //
+        // GET: /Fifteen/
+
+        public ActionResult Abril()
+        {
+            return View("~/Views/Section/Fifteen/Abril.cshtml");
+        }
+        public ActionResult Cati()
+        {
+            return View("~/Views/Section/Fifteen/Cati.cshtml");
+        }
+        public ActionResult Martina_Campos_Perez()
+        {
+            return View("~/Views/Section/Fifteen/Martina_Campos_Perez.cshtml");
+        }
+        public ActionResult Cielo()
+        {
+            return View("~/Views/Section/Fifteen/Cielo.cshtml");
+        }
+        public ActionResult Giuli()
+        {
+            return View("~/Views/Section/Fifteen/Giuli.cshtml");
+        }
+        public ActionResult Sofia_Peluso()
+        {
+            return View("~/Views/Section/Fifteen/Sofia_Peluso.cshtml");
+        }
+        public ActionResult Martina_Poveda()
+        {
+            return View("~/Views/Section/Fifteen/Martina_Poveda.cshtml");
+        }
+        public ActionResult Zoe()
+        {
+            return View("~/Views/Section/Fifteen/Zoe.cshtml");
+        }
+        public ActionResult Mica()
+        {
+            return View("~/Views/Section/Fifteen/Mica.cshtml");
+        }
+        public ActionResult Sofia()
+        {
+            return View("~/Views/Section/Fifteen/Sofia.cshtml");
+        }
+        public ActionResult Martina_Isabella()
+        {
+            return View("~/Views/Section/Fifteen/Martina_Isabella.cshtml");
+        }
+    }
+}
