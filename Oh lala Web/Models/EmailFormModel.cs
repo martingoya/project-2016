4867c06e68f546706f647c6459e97e972b3d07a8
Martin Goya <martin.goya@hotmail.com>
2016-12-09 16:18:39 -0300

    So many changes. Quality of photos, styles, etc..

--- Oh lala Web/Models/EmailFormModel.cs
+++ Oh lala Web/Models/EmailFormModel.cs
@@ -8,13 +8,13 @@ namespace Oh_lala_Web.Models
 {
     public class EmailFormModel
     {
-        [Required, Display(Name = "Your name")]
+        [Required, Display(Name = "Nombre")]
         public string FromName { get; set; }
-        [Required, Display(Name = "Your email"), EmailAddress]
+        [Required, Display(Name = "Mail"), EmailAddress]
         public string FromEmail { get; set; }
-        [Required, Display(Name = "Subject")]
+        [Required, Display(Name = "Asunto")]
         public string Subject { get; set; }
-        [Required]
+        [Required, Display(Name = "Mensaje")]
         public string Message { get; set; }
     }
 }
