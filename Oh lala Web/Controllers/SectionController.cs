ec007cf1da932718e55aed03a2375043f8c7fd5a
Martin Goya <martin.goya@hotmail.com>
2016-12-15 00:43:49 -0300

    Create database Model. Applicated model to database server.

--- Oh lala Web/Controllers/SectionController.cs
+++ Oh lala Web/Controllers/SectionController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;
+using Oh_lala_Web.Models;
 
 namespace Oh_lala_Web.Controllers
 {
     public class SectionController : Controller
     {
+        private EventEntities db = new EventEntities("OhLalaPHEntities");
 
         public ActionResult Fifteen()
         {
