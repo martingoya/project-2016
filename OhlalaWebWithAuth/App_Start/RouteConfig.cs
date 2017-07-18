3b9a9fd19536938a1bf09d84e31285be10e4594c
Martin Goya <martin.goya@hotmail.com>
2017-07-18 00:09:33 -0300

    Fix some styles and changes on how to display errors in the url

--- OhlalaWebWithAuth/App_Start/RouteConfig.cs
+++ OhlalaWebWithAuth/App_Start/RouteConfig.cs
@@ -13,10 +13,28 @@ namespace OhlalaWebWithAuth
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Accounts",
+            //    url: "Account/{action}",
+            //    defaults: new { controller = "Account", action = "Login" }
+            //);
+
+            //routes.MapRoute(
+            //    name: "Events",
+            //    url: "Loader/{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Loader", id = UrlParameter.Optional }
+            //);
+
+            //routes.MapRoute(
+            //    name: "Menu",
+            //    url: "{action}/{identifier}",
+            //    defaults: new { controller = "Home", action = "Index", identifier = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{identifier}",
+                defaults: new { controller = "Home", action = "Index", identifier = UrlParameter.Optional }
             );
         }
     }
